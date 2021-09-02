0844eb4cd2b66b6be10dfb0466c770694191789b
SANJAY SG <44039457+Sanpme66@users.noreply.github.com>
2021-09-03 00:03:58 +0530

    text

--- app.py
+++ app.py
@@ -122,6 +122,11 @@ def main():
 		st.subheader("About")
 		st.text("Built by Sanjay S G")
 		st.text("Built with Streamlit & Pandas")
+		st.markdown("""
+		This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+		* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+		* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+		""")
 		
 
 
